f3811c8c250282e5939497129e676b22ab9b30b3
mr <a@a>
2025-06-09 15:18:47 +0900

    [Modify] レビュー対応

--- Assets/MoveNokonoko.cs
+++ Assets/MoveNokonoko.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveNokonoko : MoveCharactor
+public class MoveNokonoko : MoveCharacter
 {
     private void OnTriggerExit2D(Collider2D collision)
     {
